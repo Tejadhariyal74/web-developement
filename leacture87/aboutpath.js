c2212ef610196bfb8ad4337974a3e46b30c1e434
Tejadhariyal74 <tejadhariyal@gmail.com>
2026-09-12 00:04:17 +0530

    Add files via upload

---
+++ leacture87/aboutpath.js
@@ -0,0 +1,6 @@
+import path from "path";
+// let mypath="E:\\sigma web development\\leacture87\\harry.txt"
+console.log(path)
+
+console.log(path.dirname(mypath))
+console.log(path.basename(mypath))
---
+++ leacture87/main.js
@@ -0,0 +1,18 @@
+const { error } = require("console")
+const fs=require("fs")
+// console.log(fs)
+ 
+console.log("starting ")
+
+// fs.writeFileSync("harry.txt","Harry is a good boy")
+
+fs.writeFile("harry2.txt","Harry is a good boy also",()=>{
+    console.log("done")
+    fs.readFile("harry2.txt",(error,data)=>{
+        console.log(error,data.toString())
+    })
+})
+fs.appendFile("harry.txt","harryrobo",(e,d)=>{
+    console.log(d)
+})
+console.log("ending")
